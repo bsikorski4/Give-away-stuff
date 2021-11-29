4982c7a0457de8ca6fd088a263d81561803e9a73
bsikorski4 <bartek.sikorski4@gmail.com>
2021-11-29 20:26:12 +0100

    project reupload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import "./scss/main.scss"
+import Register from "./components/Register";
+import Logout from "./components/Logout";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+        <Router>
+            <Routes>
+                <Route exact path='/' element={<Home/>}>
+                </Route>
+                <Route path='/logowanie' element={<Login/>}>
+                </Route>
+                <Route path='/rejestracja' element={<Register/>}>
+                </Route>
+                <Route path='/wylogowano' element={<Logout/>}>
+                </Route>
+            </Routes>
+        </Router>
+    )
+
 }
 
 export default App;
